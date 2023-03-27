df7ff5edc5876864fbf46b778236f56faef6bea1
osboxes.org <milktealliance@protonmail.com>
2023-03-27 20:36:23 +0100

    add search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,17 @@ const Header = () => {
     <div className="container">
       <a className="navbar-brand" href="#">
             <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                 height="30"
+                 height="60"
                  alt="MDB Logo"
                  loading="lazy" />
+                 <p>Hello</p>
         </a>
-     </div>
+        <div class="input-group">
+         <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+          <button type="button" class="btn btn-outline-primary">search</button>
+        </div>
+        
+      </div>
     </nav>
     </div>
   )
